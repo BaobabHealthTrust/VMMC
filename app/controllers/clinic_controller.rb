33b30fe0a2a80c9e1b804ecc39352a4eb1dae207
kajanesoko <starjsoko@gmail.com>
2017-10-17 14:52:18 +0200

    report UI on landing page

--- app/controllers/clinic_controller.rb
+++ app/controllers/clinic_controller.rb
@@ -44,4 +44,24 @@ class ClinicController < ApplicationController
 	def overview
 		render layout:false
 	end
+
+	def report
+		@reports =  [
+        ['/first_report','First Report'],
+        ['/second_report','Second Report']
+      ]
+		render layout:false
+	end
+
+	def first_report
+		render layout: false
+	end
+
+	def second_report
+		render layout:false
+	end
+
+	def administration
+		render layout:false
+	end
 end
