4cc5e8a1e5e1cf992f5dc5752dc37ec7e9ae6f42
mangochiman <mangochiman@gmail.com>
2017-11-06 18:00:42 +0200

    Pulling patient visits

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ Rails.application.routes.draw do
   get '/person_names/middle_name' => 'person_names#middle_name'
 
   post '/encounters/create'
+  post '/patients/get_patient_visits'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
