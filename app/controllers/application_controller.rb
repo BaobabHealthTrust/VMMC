41f813b00e31af05f4d4ee039e04a270f8d26fe4
mangochiman <mangochiman@gmail.com>
2017-11-10 12:04:28 +0200

    Printing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,13 @@ class ApplicationController < ActionController::Base
     return task
   end
 
+  def print_and_redirect(print_url, redirect_url, message = "Printing, please wait...", show_next_button = false, patient_id = nil)
+    @print_url = print_url
+    @redirect_url = redirect_url
+    @message = message
+    @show_next_button = show_next_button
+    @patient_id = patient_id
+    render template: "print/print", layout: false
+  end
+
 end
