32669085716f7700f844a30a9e550ea866331da2
kajanesoko <starjsoko@gmail.com>
2017-12-23 14:19:53 +0200

    added .sql files to be loading default user and location

---
+++ db/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`user_id`, `system_id`, `username`, `password`, `salt`, `creator`, `date_created`, `changed_by`, `person_id`, `uuid`) 
+VALUES ('1', '1-8', 'Provider', 'c60419a4689b5caed03312d3d41e0e429924e8c3', '6TnqlzzAa4', '1', NOW(), '1', '1', '48cbaafb-51f6-11e3-8fcd-003018acf05d');
+
